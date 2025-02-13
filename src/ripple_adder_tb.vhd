cfe31179b06901f63d2ee73006b08a1810a8c539
 <github noreply email@users.noreply.github.com>
2025-02-13 14:35:02 -0700

    nboooo

--- src/ripple_adder_tb.vhd
+++ src/ripple_adder_tb.vhd
@@ -54,7 +54,7 @@ begin
            assert (w_sum = x"8" and w_Cout = '0') report "carry propagation" severity failure;
         
        -- Test 3: Full Carry Propagation (e.g., 1111 + 0001)
-       w_addends <= x"F"; w_Cin <= '0'; wait for 10 ns;
+       w_addends <= x"0F"; w_Cin <= '0'; wait for 10 ns;
           assert (w_sum = x"E" and w_Cout = '0') report "oerflow carry" severity failure;
 
 		wait; -- wait forever
