7d1675f275d29ea92be257e9a7fe6491ff547c5a
 <github noreply email@users.noreply.github.com>
2025-02-12 21:26:58 -0700

    aaaaaaaaaaaahhhhhhhh

--- src/ripple_adder_tb.vhd
+++ src/ripple_adder_tb.vhd
@@ -49,17 +49,13 @@ begin
        w_addends <= x"FF"; w_Cin <= '1'; wait for 10 ns;
 	       assert (w_sum = x"F" and w_Cout = '1') report "bad with ones" severity failure;
        -- TODO, a few other test cases
-        -- Test 1: Zero Addition
-        w_addends <= x"00"; w_Cin <= '0'; wait for 10 ns;
-        assert (w_sum = x"00" and w_Cout = '0') report "Failed: Zero Addition" severity failure;
-        
         -- Test 2: Carry Propagation (e.g., 0111 + 0001)
         w_addends <= x"07"; w_Cin <= '1'; wait for 10 ns;
         assert (w_sum = x"08" and w_Cout = '0') report "Failed: Carry Propagation" severity failure;
         
         -- Test 3: Full Carry Propagation (e.g., 1111 + 0001)
-        -- w_addends <= x"0F"; w_Cin <= '0'; wait for 10 ns;
-        -- assert (w_sum = x"0E" and w_Cout = '1') report "Failed: Overflow Carry" severity failure;
+        w_addends <= x"0F"; w_Cin <= '0'; wait for 10 ns;
+        assert (w_sum = x"10" and w_Cout = '1') report "Failed: Overflow Carry" severity failure;
         
         -- Test 4: Maximum Values (Overflow)
         w_addends <= x"FF"; w_Cin <= '1'; wait for 10 ns;
